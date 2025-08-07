b9a36f9ae8362e45c9956acece1ec668bca9ffdd
Hayden Marshall <marshall.hayden65@gmail.com>
2025-08-07 15:41:27 -0500

    feat: move docker images to docker hub repo

--- bin/build.sh
+++ bin/build.sh
@@ -4,9 +4,13 @@
 echo -e "\n [#] Building client container image...\n"
 docker build -t mern-client code/client/.
 echo -e "\n [\u2713] Client container image built!\n"
+echo -e "\n [#] Pushing client container image to Docker Hub...\n"
+docker push haydenmarshall65/ihs:mern-client
 
 #build the server code
 echo -e "\n [#] Building server container\n"
 docker build -t mern-server code/server/.
 echo -e "\n [\u2713] Server container image built!\n"
+echo -e "\n [#] Pushing server container image to Docker Hub...\n"
+docker push haydenmarshall65/ihs:mern-server
 echo "Start with ./bin/start.sh!"
--- bin/start.sh
+++ bin/start.sh
@@ -6,6 +6,11 @@ if [ "$(docker ps -a -q -f name=mern-test-mongo-1)" ]; then
     echo -e "[\u2713] Old containers removed."
 fi
 
+echo "[#] Pulling latest images from Docker Hub"
+docker pull haydenmarshall65/ihs:mern-client
+docker pull haydenmarshall65/ihs:mern-server
+echo -e "[\u2713] Latest images pulled."
+
 echo "[#] Starting MERN setup"
 docker compose up -d
 echo -e "[\u2713] MERN Started!"
