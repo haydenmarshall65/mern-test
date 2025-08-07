e7d434f35409ec6db16b5104f82ea3e1dc840de6
Hayden Marshall <marshall.hayden65@gmail.com>
2025-08-07 15:42:13 -0500

    fix:force to light mode for now

--- code/client/src/components/NavBar.jsx
+++ code/client/src/components/NavBar.jsx
@@ -1,17 +1,38 @@
 import { NavLink } from "react-router-dom";
 import IhsLogo from "../assets/ihs_logo.webp"
+import { useState } from "react";
 
 export default function Navbar() {
+  const [openDropdowns, setOpenDropdowns] = useState({
+    about: false,
+    research: false,
+    learningAcademy: false,
+    partnerships: false,
+    ihsMedia: false,
+  })
   return (
     <div>
-      <nav className="flex justify-between items-center mb-6">
+      <nav className="flex justify-between items-center mb-6 bg-inherit p-4">
         <NavLink to="/">
           <img alt="IHS Company Logo" className="inline" src={IhsLogo} height="150"></img>
         </NavLink>
-
-        <NavLink className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3" to="/create">
-          Create Employee
-        </NavLink>
+        <ul className="flex space-x-4">
+          <li className="relative p-4 border-1 border-red-500 rounded-lg">
+            About
+          </li>
+          <li className="relative p-4 border-1 border-red-500 rounded-lg">
+            Research
+          </li>
+          <li className="relative p-4 border-1 border-red-500 rounded-lg">
+            Learning Academy
+          </li>
+          <li className="relative p-4 border-1 border-red-500 rounded-lg">
+            Partnerships
+          </li>
+          <li className="relative p-4 border-1 border-red-500 rounded-lg">
+            IHS Media
+          </li>
+        </ul>
       </nav>
     </div>
   );
